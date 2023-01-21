2a979010a8b9012aacee17eb599fec4d68c40448
Martino Bordin <info@martinobordin.it>
2023-01-21 14:09:19 +0100

    Dockerized WebStatus

--- src/Monitoring/WebStatus/Program.cs
+++ src/Monitoring/WebStatus/Program.cs
@@ -3,9 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddHealthChecksUI()
               .AddInMemoryStorage();
 
+
+var o = builder.Configuration.GetSection("HealthChecks-UI");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
